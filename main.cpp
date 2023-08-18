e271526789a30bc9e9c4ac434ed02e1807e0d3fe
Masahiko Toyoshiush <m.yoshi.1700@gmail.com>
2023-08-18 12:26:25 +0900

    Add data load process

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include<iostream>
+#include<string>
 
 #include "open3d/Open3D.h"
 
+using PointCloud = open3d::geometry::PointCloud;
+using PointCloudPtr = std::shared_ptr<PointCloud>;
 
-class DataLoader{
 
+namespace DataLoader{
+    PointCloudPtr load_pointcloud_ply(const std::string &filename){
+        PointCloudPtr pointcloud_ptr = PointCloudPtr(new PointCloud());
+        open3d::io::ReadPointCloudOption option;
 
+        if(open3d::io::ReadPointCloudFromPLY(filename, *pointcloud_ptr, option)){
+            std::cout << "load point cloud done" << std::endl;
 
+            if(false){
+                std::vector< std::shared_ptr< const open3d::geometry::Geometry >> vis;
+                vis.push_back(pointcloud_ptr);
+                open3d::visualization::DrawGeometries(vis);
+            }
+
+        }else{
+            std::cout << "load point cloud fail" << std::endl;
+            exit(-1);
+        }
+
+        return pointcloud_ptr;
+    }
 };
 
 
@@ -37,12 +58,11 @@ private:
 };
 
 
+
 int main(){
-    std::cerr << "hogehoge" << std::endl;
 // load point cloud
-
-
-
+    std::string pointcloud_ply_file = "./data/BunnyMesh.ply";
+    PointCloudPtr pointcloud_ptr = DataLoader::load_pointcloud_ply(pointcloud_ply_file);
 
 // define front
 
